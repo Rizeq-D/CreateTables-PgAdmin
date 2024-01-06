a7a98b8698667367e5780b4f5cab1ef89ffe2431
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-01-06 01:19:41 +0100

    Initial commit

---
+++ inserts-accounts.sql
@@ -0,0 +1,3 @@
+INSERT INTO account(username, PASSWORD, email, created_on)
+ VALUES
+ ('Jose', 'PASSWORD', 'jose@gmail.com', CURRENT_TIMESTAMP); 
---
+++ inserts-jobs.sql
@@ -0,0 +1,6 @@
+INSERT INTO job(job_name)
+-- VALUES('Barista')
+-- VALUES('Athlete')
+-- VALUES('Lawyer')
+-- VALUES('Doctor')
+VALUES('Dancer')
